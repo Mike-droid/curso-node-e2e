445b00ef69132d46fbfcd22b8f6bdeefb7f9bd57
mike-droid <miguelreyesmoreno@hotmail.com>
2024-01-24 22:43:04 -0600

    Clase 6

--- e2e/app.e2e.js
+++ e2e/app.e2e.js
@@ -1,19 +1,29 @@
 const supertest = require("supertest");
 const express = require('express');
 
-const app = express();
-
-app.get('/hello', (req, res) => res.status(200).json({name: 'mike'}));
-
-app.listen(9000);
-const api = supertest(app);
-
 describe('Tests for app', () => {
+  let app = null;
+  let server = null;
+  let api = null;
+
+  beforeEach(() => {
+    app = express();
+
+    app.get('/hello', (req, res) => res.status(200).json({name: 'mike'}));
+
+    server = app.listen(9000);
+    api = supertest(app);
+  });
+
   test('GET /hello', async () => {
     const response = await api.get('/hello');
     expect(response).toBeTruthy();
     expect(response.statusCode).toEqual(200);
     expect(response.body.name).toEqual('mike');
     expect(response.headers['content-type']).toMatch(/json/);
+  });
+
+  afterEach(() => {
+    server.close();
   });
 });
