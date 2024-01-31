88d521f1fdcc46c3129aa534aa6bf4cdc167311a
mike-droid <miguelreyesmoreno@hotmail.com>
2024-01-30 21:51:26 -0600

    Clase 12

--- e2e/app.e2e.js
+++ e2e/app.e2e.js
@@ -1,5 +1,6 @@
-const supertest = require("supertest");
+const supertest = require('supertest');
 const createApp = require('../src/app');
+const { config } = require('../src/config/config');
 
 describe('Tests for app', () => {
   let app = null;
@@ -12,12 +13,33 @@ describe('Tests for app', () => {
     api = supertest(app);
   });
 
-  test('GET /hello', async () => {
-    const response = await api.get('/hello');
-    expect(response).toBeTruthy();
-    expect(response.statusCode).toEqual(200);
-    expect(response.body.name).toEqual('mike');
-    expect(response.headers['content-type']).toMatch(/json/);
+  describe('GET /hello', () => {
+    test('should return correct info  when requesting hello route', async () => {
+      const response = await api.get('/hello');
+      expect(response).toBeTruthy();
+      expect(response.statusCode).toEqual(200);
+      expect(response.body.name).toEqual('mike');
+      expect(response.headers['content-type']).toMatch(/json/);
+    });
+  });
+
+  describe('GET /nueva-ruta', () => {
+    test('should return status code of 401 without api key', async () => {
+      const { statusCode } = await api.get('/nueva-ruta');
+      expect(statusCode).toEqual(401);
+    });
+
+    test('should return status code of 401 with invalid api key', async () => {
+      const { statusCode } = await api.get('/nueva-ruta').set({ api: 'nope' });
+      expect(statusCode).toEqual(401);
+    });
+
+    test('should return status code of 200 with correct api key', async () => {
+      const { statusCode } = await api
+        .get('/nueva-ruta')
+        .set({ api: config.apiKey });
+      expect(statusCode).toEqual(200);
+    });
   });
 
   afterAll(() => {
