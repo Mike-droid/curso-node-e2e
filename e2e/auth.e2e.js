9818f69c246641f855668b9b758e159a5a9182cc
mike-droid <miguelreyesmoreno@hotmail.com>
2024-02-17 10:00:42 -0600

    Clase 23

--- e2e/auth.e2e.js
+++ e2e/auth.e2e.js
@@ -3,6 +3,18 @@ const createApp = require('../src/app');
 const { models } = require('../src/db/sequelize');
 const { upSeed, downSeed } = require('./utils/umzug');
 
+const mockSendMail = jest.fn();
+
+jest.mock('nodemailer', () => {
+  return {
+    createTransport: jest.fn().mockImplementation(() => {
+      return {
+        sendMail: mockSendMail,
+      };
+    }),
+  };
+});
+
 describe('Tests for /auth path', () => {
   let app = null;
   let server = null;
@@ -41,6 +53,37 @@ describe('Tests for /auth path', () => {
       expect(body.access_token).toBeTruthy();
       expect(body.user.email).toEqual(user.email);
       expect(body.user.password).toBe(undefined);
+    });
+  });
+
+  describe('POST /recovery', () => {
+
+    beforeAll(() => {
+      mockSendMail.mockClear();
+    });
+
+    test('should return a 401 with invalid password', async () => {
+      const inputData = {
+        email: 'emailfake@mail.com',
+      };
+      const { statusCode } = await api
+        .post(`${apiUrlBase}recovery`)
+        .send(inputData);
+      expect(statusCode).toBe(401);
+    });
+
+    test('should send a mail', async () => {
+      const user = await models.User.findByPk('1');
+      const inputData = {
+        email: user.email,
+      };
+      mockSendMail.mockResolvedValue(true);
+      const { statusCode, body } = await api
+        .post(`${apiUrlBase}recovery`)
+        .send(inputData);
+      expect(statusCode).toBe(200);
+      expect(body.message).toEqual('mail sent');
+      expect(mockSendMail).toHaveBeenCalled();
     });
   });
 
